7c3016f44c6f90f7f67e6525bff8395be517d4b8
Hugo Jobling <hello@thisishugo.com>
2019-07-07 16:29:25 +0100

    Add large screen top nav elements

--- src/TopNav.js
+++ src/TopNav.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { Flex } from "rebass";
+import { Box, Flex } from "rebass";
 
+import Adelle from "./Adelle";
 import Container from "./Container";
+import HideSmall from "./HideSmall";
 
 import logoIcon from "./svg/logo-icon.svg";
+import logoHorizontal from "./svg/logo-horizontal.svg";
 import userIcon from "./svg/user-icon.svg";
 
 export default function TopNav() {
   return (
     <Container p={3}>
       <Flex justifyContent="space-between">
-        <img src={logoIcon} />
-        <img src={userIcon} />
+        <Box>
+          <picture>
+            <source media="(min-width: 30rem)" srcSet={logoHorizontal} />
+            <img src={logoIcon} alt="Deliveroo logo" />
+          </picture>
+        </Box>
+
+        <Flex alignItems="center">
+          <img src={userIcon} />
+          <HideSmall>
+            <Adelle mt={1} ml={2} color="darkgrey" fontSize={1}>
+              Jane Smith
+            </Adelle>
+          </HideSmall>
+        </Flex>
       </Flex>
     </Container>
   );
